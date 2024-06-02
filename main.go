d87ebc7952850c2e271f68efc28c5f7a532cd575
Samuel Boyden <sam@stboyden.com>
2024-06-02 21:32:30 +0100

    move to a http mux handler
    - add error page handling
    - add temporary storage directory for future image storage

--- main.go
+++ main.go
@@ -3,18 +3,44 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"image-ranker/components"
-
-	"github.com/a-h/templ"
 )
 
 func main() {
+	tmpStorage, err := os.MkdirTemp("", "image-ranker-runtime-images")
+	if err != nil {
+		panic("could not create temporary directory: " + err.Error())
+	}
+
 	root := components.Root
 
-	http.Handle("/", templ.Handler(root(components.Index())))
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		path := r.URL.Path
+		context := r.Context()
+
+		log.Printf("requested %s", path)
+
+		switch path {
+		case "/":
+			log.Println("serving /")
+			_ = root(components.Index()).Render(context, w)
+		default:
+			log.Printf("serving 404 for path %s", path)
+			_ = root(components.Error(404, fmt.Sprintf("Path \"%s\" not found", path))).Render(context, w)
+		}
+	})
+
 	log.Println("Listening on http://localhost:3000")
-	_ = http.ListenAndServe(":3000", nil)
+	_ = http.ListenAndServe(":3000", mux)
+
+	err = os.RemoveAll(tmpStorage)
+	if err != nil {
+		panic("could not remove temporary directory: " + err.Error())
+	}
 }
